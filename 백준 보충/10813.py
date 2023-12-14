62a29945fccfe71d5d0efddb7337f09f41176785
홍길동 <@naver.com>
2023-12-14 22:33:18 +0900

    2023

---
+++ 백준 보충/10813.py
@@ -0,0 +1,16 @@
+n,m = map(int, input().split())
+list = []
+
+for i in range(1,n+1):
+    list.append(i)
+
+for i in range(m):
+    a,b = map(int, input().split())
+    temp = list[a-1]
+    list[a-1] = list[b-1]
+    list[b-1] = temp
+
+for b in list:
+    print(b, end = ' ')
+
+
