909ddc69e059638a95728786c8750264b8aacbc0
홍길동 <fofn0766@gmail.com>
2023-12-29 00:59:44 +0900

    2

--- 백준 보충/2805.py
+++ 백준 보충/2805.py
@@ -2,7 +2,21 @@ import sys
 input = sys.stdin.readline
 
 a,b = map(int, input().split())
-list = []
+trees = list(map(int, input().split()))
+start, end = 0, max(trees)
 
-c,d,e,f = (map(int, input().split()))
+while start <= end:
+    mid = (start + end)//2
+    total = 0
+
+    for tree in trees:
+        if tree >= mid:
+            total += tree - mid
+
+    if total >= b:
+        start = mid + 1
+    else:
+        end = mid -1
+
+print(end)
 
--- 백준 보충/8595.py
+++ 백준 보충/8595.py
@@ -17,7 +17,8 @@ print(sum(k))
 n = int(input())
 s = input()
 numbers = set("1234567890")  # 숫자를 검사할 때 set을 사용하면 빠르게 확인 가능합니다.
-
+// c if c in numbers else ' ': 만약 문자 c가 numbers 집합에 속한다면 c를 선택하고, 그렇지 않으면 공백 ' '을 선택
+// for c in s: 문자열 s의 각 문자 c에 대해 위의 표현식을 수행
 tmps = ''.join([c if c in numbers else ' ' for c in s])  # 리스트 컴프리헨션과 join을 사용하여 효율적으로 문자열을 만듭니다.
 
 print(sum(map(int, tmps.split())))
