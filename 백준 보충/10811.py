b0fccba7c79dbdb51ddf86acc1de5ef9b515ddd5
홍길동 <fofn0766@gmail.com>
2024-01-05 20:52:12 +0900

    @

---
+++ 백준 보충/10811.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+a,b = map(int, input().split())
+line = [i for i in range(a+1)]
+
+for j in range(b):
+    c,d = map(int, input().split())
+    #확실히 문자열/인덱싱 하는 것에 엄청난 약점을 지니고 있음. 
+    temp = line[c-1:d]
+    temp.reverse()
+    line[c-1:d] = temp
+
+for j in range(a):
+  print(line[j],end=" ")
+
+
+
