2f65957f37bd2e14a3159274cd72f7bfe2670f1b
홍길동 <fofn0766@gmail.com>
2024-01-23 16:38:24 +0900

    2

--- 1406.py
+++ 1406.py
@@ -1,7 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n =input()
-m - int(input())
+n = input().strip()  # 문자열 양쪽의 공백을 제거하여 n을 공백 없는 문자열로 만듭니다.
+m = int(input())
+lst = []
+
 for i in range(m):
-    
+    lst.append(input().split())
+
+for j in lst:
+    if j[0] == 'P':
+        n = ''.join(n + j[1])
+    elif j[0] == 'L':
+        
+
+
--- 20055.py
+++ 20055.py
@@ -1,6 +1,30 @@
-N,K = map(int, input().split())
-container = []
+from collections import deque
 
-for i in range(2*N):
-    container.append(int(input()))
+N,K = map(int, input().split())
+belt = deque(list(map(int, input().split())))
+robot = deque(0 for _ in range(2*N))
+
+count = 0
+
+while True:
+    belt.rotate(1)
+    robot.rotate(1)
+    robot[N-1] = 0
+
+    for i in range(N-2,-1,-1):
+        if (robot[i] == 1) and (robot[i+1] == 0) and (belt[i+1] != 0):
+            robot[i] = 0
+            robot[i+1] = 1
+            belt[i+1] -= 1
+        robot[N-1] = 0
+
+    if belt[0] != 0:
+        robot[0] = 1
+        belt[0] -= 1
+    
+    count += 1
+
+    if belt.count(0) >= K:
+        break
+print(count)
     
