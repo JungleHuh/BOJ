efebc1419882b51bfbc9113279bd43bb7136fc50
홍길동 <fofn0766@gmail.com>
2024-02-02 11:16:11 +0900

    2

--- 14499.py
+++ 14499.py
@@ -9,23 +9,23 @@ dice = [0]* 6
 dx = [0,0,-1,1]
 dy = [1,-1,0,0]
 
-for i in command:
 
-    if i == 1:
+for i in range(K):
+    dir = command[i] - 1
+    nx = x + dx[dir]
+    ny = y + dy[dir]
+    if not 0 <= nx < N or not 0 <= ny < M:
+        continue
+
+    if dir == 0:
         dice[0], dice[2], dice[3], dice[5] = dice[3], dice[0], dice[5], dice[2]
-    elif i == 2:
+    elif dir == 1:
         dice[0], dice[2], dice[3], dice[5] =  dice[2], dice[5], dice[0], dice[3]
-    elif i == 3:
+    elif dir == 2:
         dice[0], dice[1], dice[4], dice[5] = dice[4], dice[0], dice[5], dice[1]
     else:
         dice[0], dice[1], dice[4], dice[5] = dice[1], dice[5], dice[0], dice[4]
     
-    nx = x + dx[i-1]
-    ny = y + dy[i-1]
-
-    if nx >= N or nx < 0 or ny >= M or ny < 0:
-        continue
-
     if maps[nx][ny] == 0:
         maps[nx][ny] = dice[5]
     else:
