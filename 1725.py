bd4de03c5f680d071a123b983345b8b20caef3ab
홍길동 <fofn0766@gmail.com>
2024-03-12 23:55:02 +0900

    2

--- 1725.py
+++ 1725.py
@@ -1,41 +1,40 @@
-while True:
-    bars = []
-    while True:
-        bar_input = input().strip()
-        bar = int(bar_input)
-        if bar == 0:
-            break
-        bars.append(bar)
+import sys
+input = sys.stdin.readline
 
-    if len(bars) == 0:
-        break
+# 막대 개수
+bar_count = int(input())
 
-    stack = []
-    num_bars = len(bars)
-    max_area = 0
+# 막대 높이 리스트
+bar_heights = []
+for _ in range(bar_count):
+  height = int(input())
+  bar_heights.append(height)
 
-    for index in range(num_bars):
-        if not stack: 
-            stack.append((index, bars[index]))
-        else:
-            if stack[-1][1] <= bars[index]:
-                stack.append((index, bars[index]))
-            else:
-                while stack and stack[-1][1] > bars[index]:
-                    popped_bar = stack.pop()
-                    if not stack:
-                        width = index
-                    else:
-                        width = index - stack[-1][0] - 1
-                    max_area = max(max_area, popped_bar[1] * width)
-                stack.append((index, bars[index]))
+# 스택
+stack = []
 
-    while stack:
-        popped_bar = stack.pop()
-        if len(stack) == 0:
-            width = num_bars
-        else:
-            width = num_bars - stack[-1][0]
-        max_area = max(max_area, popped_bar[1] * width)
+# 최대 면적
+max_area = 0
 
-    print(max_area)
+# 각 막대 순회
+for i in range(bar_count):
+  current_index = i
+
+  # 스택에 값이 존재하면 큰 값을 만날 때까지 pop
+  while stack and stack[-1][1] > bar_heights[i]:
+    popped_index, popped_height = stack.pop()
+    # 현재 인덱스와 해당 인덱스까지의 차이와 높이를 곱해서 최대 넓이를 탐색
+    area = (i - popped_index) * popped_height
+    max_area = max(max_area, area)
+
+  # 현재 막대 정보 스택에 추가
+  stack.append((current_index, bar_heights[i]))
+
+# 전체가 동일한 높이일 경우를 추가적으로 고려
+while stack:
+  popped_index, popped_height = stack.pop()
+  area = (bar_count - popped_index) * popped_height
+  max_area = max(max_area, area)
+
+# 최대 면적 출력
+print(max_area)
